f5a8ee7881ce840ec9e00789ed1b188334ed74f8
Kittipot Buaket <kittipot1999@hotmail.com>
2021-08-05 14:27:18 +0700

    search

--- script.js
+++ script.js
@@ -5,7 +5,21 @@ const length = document.getElementById('length')
 const color = document.querySelector('#color')
 
 // define more constants and variables here
+function search()
+{
+  let textTosearch = document.getElementById
+  ("text to search").value;
+  let paragraph = document.getElementById
+  ("paragraph");
+  textTosearch = textTosearch.replace(/[.*+?^%${}()| [\]\\]/g,"//$&");
 
+  let pattern = new RegExp(`${textTosearch}`,"gi");
+
+  paragraph.innerHTML =paragraph.textContent.
+  replace(pattern, match => `<mark>${match}</mark>`)
+
+  
+}
 btn_toggle.onclick = () => {
   // your code here
 }
